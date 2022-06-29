a179e82086a34efb9022cdffba79719ea9b6972a
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2022-06-29 21:49:56 +0900

    wad

--- seungmin/processing_data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import os
-import cv2
-
-print(os.listdir("/Users/hongseongmin/Desktop/data_image/"))
-
-img = cv2.imread("/Users/hongseongmin/Desktop/data_image/test0.png")
-print(img.shape)
